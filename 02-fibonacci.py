fbf1e61059791724a5e0333326ed3cf19cf173a8
Daxkys <migueldinizp@gmail.com>
2022-06-08 02:03:34 +0200

    reto 2: fibonacci

---
+++ 02-fibonacci.py
@@ -0,0 +1,21 @@
+'''
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 17/01/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+'''
+
+num1 = 0
+num2 = 1
+
+for x in range(1,51):
+    print(x,'-',num1)
+
+    sum_fib = num1 + num2
+    num1 = num2
+    num2 = sum_fib
