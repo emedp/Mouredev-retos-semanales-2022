fee4d7a3ee7ef7cff32095927b056c4bba63a724
Daxkys <migueldinizp@gmail.com>
2022-06-08 02:04:18 +0200

    reto 5: aspect ratio

---
+++ 05-aspect_ratio.py
@@ -0,0 +1,37 @@
+'''
+ * Reto #5
+ * ASPECT RATIO DE UNA IMAGEN
+ * Fecha publicación enunciado: 01/02/22
+ * Fecha publicación resolución: 07/02/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Crea un programa que se encargue de calcular el aspect ratio de una imagen a partir de una url.
+ * - Nota: Esta prueba no se puede resolver con el playground online de Kotlin. Se necesita Android Studio.
+ * - Url de ejemplo: https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png
+ * - Por ratio hacemos referencia por ejemplo a los "16:9" de una imagen de 1920*1080px.
+
+ * - MODIFICACION PROPIA: introduciendo una cantidad de pixeles, muestra el parejo para componer los distintos ratios de aspecto.
+'''
+
+
+def aspect_ratio(pixeles):
+    pixeles = int(pixeles)
+    print('1:1 Square')
+    print(pixeles, 'px,', pixeles, 'px','\n')
+
+    print('4:3 standar')
+    print(pixeles, 'px,', (pixeles * 3 / 4), 'px','\n')
+    
+    print('16:10 standar')
+    print(pixeles, 'px,', (pixeles * 10 / 16), 'px','\n')
+    
+    print('16:9 standar')
+    print(pixeles, 'px,', (pixeles * 9 / 16), 'px','\n')
+    
+    print('9:16 Vertical')
+    print(pixeles,'px,', (pixeles * 16 / 9), 'px','\n')
+    
+    
+
+
+aspect_ratio(input('Introduce los px para calcular sus distintos "aspect ratio": '))
